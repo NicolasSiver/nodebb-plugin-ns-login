0e9d0c5da21e7b28b712c318e17944be73981f96
Barış Soner Uşaklı <barisusakli@gmail.com>
2019-01-10 23:31:33 -0500

    remove missing module

--- plugin/nodebb.js
+++ plugin/nodebb.js
@@ -4,7 +4,6 @@
     Module.exports = {
         adminSockets : NodeBB.require('./src/socket.io/admin').plugins,
         db           : NodeBB.require('./src/database'),
-        emitter      : NodeBB.require('./src/emitter'),
         meta         : NodeBB.require('./src/meta'),
         password     : NodeBB.require('./src/password'),
         pluginSockets: NodeBB.require('./src/socket.io/plugins'),
